cf31aef7eb8bbb0de175161455b4313f7d81f862
Quang Hoa <hoatq@namviet-corp.vn>
2018-05-05 23:06:44 +0700

    edit

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,6 @@
-function sortHeaders(n, t) {
-  var i = n.name.toLowerCase(),
-    r = t.name.toLowerCase();
-  return i < r ? -1 : i > r ? 1 : 0;
-}
 
-function getStyle(n) {
-  var t = "";
-  return (
-    n.match(/(200|201|202|203|204|205|206|207)/g) !== null && (t = "success"),
-    n.match(/(300|301|302|303|304|305|306|307)/g) !== null && (t = "info"),
-    n.match(
-      /(400|401|402|403|404|405|406|407|408|409|410|411|412|413|414|415|416|417)/g
-    ) !== null && (t = "error"),
-    n.match(/(500|501|502|503|504|505)/g) !== null && (t = "error"),
-    t
-  );
-}
 $(function() {
-  chrome.tabs.getSelected(function(n) {
+  chrome.tabs.getSelected(null,function(n) {
     var i = chrome.extension.getBackgroundPage().headerInfo[n.id],
       t,
       r,
@@ -27,9 +10,11 @@ $(function() {
       e,
       o;
     if (i == undefined)
+    {
       $("#result").html(
         "<b><br />Could not retrieve any headers, try reload the tab to view the headers.<br />chrome:// pages and Chrome Store don't provide any response headers.<br /><br /></b>"
       );
+    }
     else {
       for (console.log(i), t = "", r = 0, s = i.response.length; r < s; ++r) {
         for (
